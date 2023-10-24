8d90d296dc2de9779ec7593c72273160db16850a
Fmquito45 <fmquitox45@gmail.com>
2023-10-24 01:08:19 -0400

    adds an eventhandler to the beforeinstallprompt

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -25,11 +25,26 @@ export const putDb = async (content) => {
     const result = await request;
     console.log('saved to the database', result);
   } catch (error) {
-    console.error("putDb not implemented", error);
+    console.error("putDb not implemented");
   }
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () =>  {
+  try {
+    console.log("get from the database");
+
+    const jateDB = await openDB("jate", 1);
+    const tx = jateDB.transaction("jate", "readonly");
+    const store = tx.objectStore("jate");
+    const request = store.get(1);
+
+    const result = await request;
+    console.log("result.value", result);
+    return result.value;
+  } catch (err) {
+    console.error("getDb not implemented");
+  }
+};
 
 initdb();
