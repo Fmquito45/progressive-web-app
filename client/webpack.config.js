64a0f1b571077a97682590d62123ed989e613976
Fmquito45 <fmquitox45@gmail.com>
2023-10-24 01:43:39 -0400

    adds css loaders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -50,6 +50,10 @@ module.exports = () => {
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
         
       ],
     },
